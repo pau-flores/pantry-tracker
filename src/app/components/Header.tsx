5dde2c6e7e261e7da250f6de449e99826a3d7a06
Paulina Flores <paufr.2822@gmail.com>
2024-08-04 07:43:47 -0700

    Fixed header structure

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,17 +1,53 @@
+"use client";
+
 import Link from "next/link";
-import { AppBar, Toolbar, Typography, Button, Grid } from "@mui/material";
+import { Typography, Button, Grid } from "@mui/material";
+import { useAuth } from "../context/AuthContext";
 
 export default function Header() {
+  const { currentUser, logout } = useAuth();
+
+  const handleLogout = async () => {
+    await logout();
+  };
+
   return (
-    <Grid container>
-      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-        PT
-      </Typography>
-      <Button color="inherit">
-        <Link href="/pantry" passHref>
-          <Typography color="inherit">My Pantry</Typography>
+    <Grid container sx={{ py: 2 }}>
+      <Typography
+        variant="h6"
+        component="div"
+        sx={{ flexGrow: 1, fontWeight: "bold", textTransform: "uppercase" }}
+      >
+        <Link href="/" passHref>
+          <span style={{ color: "#1B263B" }}>Pantry</span>
+          <span style={{ color: "#A60067" }}>Tracker</span>
         </Link>
-      </Button>
+      </Typography>
+      {currentUser && (
+        <>
+          <Button
+            sx={{
+              mr: 2,
+              color: "#1B263B",
+              "&:hover": { color: "#A60067", backgroundColor: "transparent" },
+            }}
+          >
+            <Link href="/pantry" passHref>
+              <Typography color="inherit">My Pantry</Typography>
+            </Link>
+          </Button>
+          <Button
+            sx={{
+              mr: 2,
+              color: "#1B263B",
+              "&:hover": { color: "#A60067", backgroundColor: "transparent" },
+            }}
+            onClick={handleLogout}
+          >
+            <Typography color="inherit">Sign Out</Typography>
+          </Button>
+        </>
+      )}
     </Grid>
   );
 }
